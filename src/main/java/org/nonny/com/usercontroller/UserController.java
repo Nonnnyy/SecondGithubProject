c5450e8a089b7e9bf124b6f0e4e054ea7929063c
Nonnnyy <Nonnny07@gmail.com>
2022-08-08 12:02:50 +0100

    user home rests api completed

--- src/main/java/org/nonny/com/usercontroller/UserController.java
+++ src/main/java/org/nonny/com/usercontroller/UserController.java
@@ -1,14 +1,17 @@
 package org.nonny.com.usercontroller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
 	
+	@GetMapping("/home")
 	public String home() {
 		
+	
 		return "Welcome to USER HOME PAGE";
 	}
 
